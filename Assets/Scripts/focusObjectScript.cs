505e2c7e7af7e29422dd5b8bbf1e9efe39e82e9d
bbawuah <brianbawuah@icloud.com>
2021-09-29 15:54:55 +0200

    Change pattern over time

--- Assets/Scripts/focusObjectScript.cs
+++ Assets/Scripts/focusObjectScript.cs
@@ -9,21 +9,35 @@ public class focusObjectScript : MonoBehaviour
     public float amplitude;
     public float frequency;
 
+    public float x;
+    public float y;
+    public float z;
+
     // Start is called before the first frame update
     void Start()
     {
-  
+   
     }
 
     // Update is called once per frame
     void Update()
     {
-        float x = Mathf.Cos(Time.time * frequency) * amplitude;
-        float y = Mathf.Sin(Time.time * frequency) * amplitude;
-        float z = transform.position.z;
-
-
         transform.position = new Vector3(x, y, z);
-        
+
+        x = Mathf.Sin(Time.time * frequency) * amplitude;
+        y = transform.position.y;
+        z = transform.position.z;
+
+        if (Time.time > 40.0)
+        {
+            x = transform.position.x;
+            y = Mathf.Sin(Time.time * frequency) * amplitude;
+        }
+
+        if (Time.time > 80.0)
+        {
+            x = Mathf.Cos(Time.time * frequency) * amplitude; 
+            y = Mathf.Sin(Time.time * frequency) * amplitude;
+        }
     }
 }
