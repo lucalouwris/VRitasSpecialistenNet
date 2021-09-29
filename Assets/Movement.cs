f02949135551f1012f0c5d8286f71b5c350c2aa8
trymstenberg <55834832+trymstenberg@users.noreply.github.com>
2021-09-29 12:58:12 +0200

    Updated steamVR and adde movement script

---
+++ Assets/Movement.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Got from https://forum.unity.com/threads/solved-random-wander-ai-using-navmesh.327950/
+
+public class Movement : MonoBehaviour
+{
+    public float wanderRadius;
+    public float wanderTimer;
+
+    private Transform target;
+    private UnityEngine.AI.NavMeshAgent agent;
+    private float timer;
+
+    // Use this for initialization
+    void OnEnable()
+    {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        timer = wanderTimer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= wanderTimer)
+        {
+            Vector3 newPos = RandomNavSphere(transform.position, wanderRadius, -1);
+            agent.SetDestination(newPos);
+            timer = 0;
+        }
+    }
+
+    public static Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask)
+    {
+        Vector3 randDirection = Random.insideUnitSphere * dist;
+
+        randDirection += origin;
+
+        UnityEngine.AI.NavMeshHit navHit;
+
+        UnityEngine.AI.NavMesh.SamplePosition(randDirection, out navHit, dist, layermask);
+
+        return navHit.position;
+    }
+}
