7ae59977c9d0f7ea9b3ffe126f9fca047a93a1ab
bbawuah <brianbawuah@icloud.com>
2021-09-30 14:15:41 +0200

    More patterns

--- Assets/Scripts/focusObjectScript.cs
+++ Assets/Scripts/focusObjectScript.cs
@@ -9,9 +9,9 @@ public class focusObjectScript : MonoBehaviour
     public float amplitude;
     public float frequency;
 
-    public float x;
-    public float y;
-    public float z;
+    private float x;
+    private float y;
+    private float z;
 
     // Start is called before the first frame update
     void Start()
@@ -22,22 +22,82 @@ public class focusObjectScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(x, y, z);
 
+        this.HandlePattern();
+
+        transform.position = new Vector3(x, y, z);
+    }
+
+    void ReversedLinearPattern()
+    {
         x = Mathf.Sin(Time.time * frequency) * amplitude;
         y = transform.position.y;
         z = transform.position.z;
+    }
 
-        if (Time.time > 40.0)
+    void LinearPattern()
+    {
+        x = transform.position.x;
+        y = Mathf.Sin(Time.time * frequency) * amplitude;
+    }
+
+    void CirclePattern()
+    {
+        x = Mathf.Sin(Time.time * frequency) * amplitude;
+        y = Mathf.Cos(Time.time * frequency) * amplitude;
+    }
+
+    void KnotPattern()
+    {
+        x = Mathf.Sin(Time.time * frequency) * amplitude;
+        y = Mathf.Cos(Time.time * frequency) * Mathf.Sin(Time.time * frequency) * amplitude;
+    }
+
+    void ReveredKnotPattern()
+    {
+        x = Mathf.Cos(Time.time * frequency) * Mathf.Sin(Time.time * frequency) * amplitude;
+        y = Mathf.Sin(Time.time * frequency) * amplitude;
+    }
+
+    void WeirdPattern()
+    {
+        x = Mathf.Sin(Time.time * frequency) * amplitude;
+        y = Mathf.Cos(Time.time * frequency) * Mathf.Sin(Time.time * frequency) * amplitude;
+    }
+
+    void HandlePattern()
+    {
+
+        if (Time.time < 10.0)
         {
-            x = transform.position.x;
-            y = Mathf.Sin(Time.time * frequency) * amplitude;
+            this.ReversedLinearPattern();
         }
 
-        if (Time.time > 80.0)
+        if (Time.time > 10.0 && Time.time < 30.0)
         {
-            x = Mathf.Cos(Time.time * frequency) * amplitude; 
-            y = Mathf.Sin(Time.time * frequency) * amplitude;
+            this.LinearPattern();
+        }
+
+        if (Time.time > 30.0 && Time.time < 40.0)
+        {
+            this.CirclePattern();
+        }
+
+
+        if (Time.time > 40.0 && Time.time < 50.0)
+        {
+            this.KnotPattern();
+        }
+
+
+        if (Time.time > 50.0 && Time.time < 60.0)
+        {
+            this.ReveredKnotPattern();
+        }
+
+        if (Time.time > 60.0 && Time.time < 70.0)
+        {
+            this.WeirdPattern();
         }
     }
 }
