424b2cd279679ca32933fe32abfeaa2dad2805f9
LDvanGrinsven <61006226+LDvanGrinsven@users.noreply.github.com>
2021-10-28 15:10:48 +0200

    Removed unused code.

--- Assets/Scripts/Player/WalkingController.cs
+++ Assets/Scripts/Player/WalkingController.cs
@@ -6,11 +6,6 @@ public class WalkingController : MonoBehaviour
 {
     [SerializeField] ControllerInput leftController;
     [SerializeField] ControllerInput rightController;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
