cbeae9026edbc916b0a5bd0bcb5307b4ad823a7f
LDvanGrinsven <61006226+LDvanGrinsven@users.noreply.github.com>
2021-10-28 14:55:24 +0200

    Fixed the issue where you would walk through the floor when walking and looking up or down.

--- Assets/Scripts/Player/WalkingController.cs
+++ Assets/Scripts/Player/WalkingController.cs
@@ -23,8 +23,11 @@ public class WalkingController : MonoBehaviour
             rightController.previousPosition = rightController.transform.position;
             rightController.c_Movement = (Vector3.Distance(rightController.currentPosition, rightController.previousPosition) / Time.deltaTime) / 100;
 
+            Vector3 direction = Camera.main.transform.forward;
+            direction.y = 0;
+
             float moveDistance = leftController.c_Movement + rightController.c_Movement;
-            transform.position += (Camera.main.transform.forward * moveDistance);
+            transform.position += (direction * moveDistance);
 
             leftController.currentPosition = leftController.transform.position;
             rightController.currentPosition = rightController.transform.position;
