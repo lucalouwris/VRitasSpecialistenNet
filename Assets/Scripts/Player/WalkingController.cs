6c9c3ff0b0c851d2f02ff58782dc9597ebad43a2
LDvanGrinsven <61006226+LDvanGrinsven@users.noreply.github.com>
2021-11-01 11:51:36 +0100

    Fixed the issue where you would teleport between walking as the distance would not update unless holding grips which made the transition not smooth.

--- Assets/Scripts/Player/WalkingController.cs
+++ Assets/Scripts/Player/WalkingController.cs
@@ -10,22 +10,21 @@ public class WalkingController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        leftController.previousPosition = leftController.transform.position;
+        leftController.c_Movement = (Vector3.Distance(leftController.currentPosition, leftController.previousPosition) / Time.deltaTime) / 100;
+
+        rightController.previousPosition = rightController.transform.position;
+        rightController.c_Movement = (Vector3.Distance(rightController.currentPosition, rightController.previousPosition) / Time.deltaTime) / 100;
+
         if (leftController.isWalking || rightController.isWalking)
         {
-            leftController.previousPosition = leftController.transform.position;
-            leftController.c_Movement = (Vector3.Distance(leftController.currentPosition, leftController.previousPosition) / Time.deltaTime) / 100;
-
-            rightController.previousPosition = rightController.transform.position;
-            rightController.c_Movement = (Vector3.Distance(rightController.currentPosition, rightController.previousPosition) / Time.deltaTime) / 100;
-
             Vector3 direction = Camera.main.transform.forward;
             direction.y = 0;
 
             float moveDistance = leftController.c_Movement + rightController.c_Movement;
             transform.position += (direction * moveDistance);
-
-            leftController.currentPosition = leftController.transform.position;
-            rightController.currentPosition = rightController.transform.position;
         }
+        leftController.currentPosition = leftController.transform.position;
+        rightController.currentPosition = rightController.transform.position;
     }
 }
