6c10e38681dee77bf900776f6f36929932fc0c78
Luca Louwris <luca.louwris@gmail.com>
2021-11-02 10:12:57 +0100

    Added a timer that uses a trigger collider

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Timeline.Actions;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    private bool timerStarted = false;
+    [SerializeField] private float timerLength = 12000;
+    
+    /// <summary>
+    /// Code that runs every frame.
+    /// </summary>
+    private void Update()
+    {
+        // Simple count down timer
+        if (timerStarted)
+        {
+            timerLength -= Time.deltaTime;
+        }
+        
+        // Checking if timer ran out
+        if (timerLength < 0)
+        {
+            Debug.Log("Time is up.");
+            timerLength = 0;
+            timerStarted = false;
+        }
+    }
+
+    /// <summary>
+    /// Code that triggers when player opens the door.
+    /// </summary>
+    /// <param name="other"></param>
+    private void OnTriggerEnter(Collider other)
+    {
+        // Checking if colliding object is the XR Rig
+        if (other.CompareTag("MainCamera"))
+        {
+            Debug.Log(timerLength);
+            timerStarted = true;
+        }
+    }
+}
