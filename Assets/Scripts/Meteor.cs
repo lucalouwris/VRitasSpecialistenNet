930e399705363f69773c90e32cc3fcbfbab5b9d0
DESKTOP-DVO1LLP\VR Student 1 <brian.bawuah@hva.nl>
2021-11-03 13:17:33 +0100

    Added rotation to meteor

--- Assets/Scripts/Meteor.cs
+++ Assets/Scripts/Meteor.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class Meteor : MonoBehaviour
 {
+    private float range;
 
+   void Start()
+    {
+        range = Random.Range(0.35f, 2.5f);
+    }
 
     // Update is called once per frame
     void Update()
     {
-        this.transform.Rotate(Mathf.Sin(Random.Range(0.35f, 5.0f) * Time.time), Mathf.Sin(Random.Range(0.35f, 5.0f) * Time.time), Mathf.Sin(Random.Range(0.35f, 5.0f) * Time.time));
+        this.transform.Rotate(Mathf.Sin(range * Time.time), 0, 0);
     }
 }
