c271e3fafa3843aed9278b2dfb19a8a5f198c759
LDvanGrinsven <61006226+LDvanGrinsven@users.noreply.github.com>
2021-11-03 11:05:45 +0100

    Fixed minor bug with the sign rotation

--- Assets/Scripts/Interactions/SignRotation.cs
+++ Assets/Scripts/Interactions/SignRotation.cs
@@ -14,14 +14,16 @@ public class SignRotation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float distance = Vector3.Distance(this.transform.position, player.transform.position);
-        if(player != null && distance < 20)
+        if(player != null)
         {
-            Vector3 targetPostition = new Vector3(player.transform.position.x,
-                                       this.transform.position.y,
-                                       player.transform.position.z);
-            this.transform.LookAt(targetPostition);
+            float distance = Vector3.Distance(this.transform.position, player.transform.position);
+            if (distance < 20)
+            {
+                Vector3 targetPostition = new Vector3(player.transform.position.x,
+                                           this.transform.position.y,
+                                           player.transform.position.z);
+                this.transform.LookAt(targetPostition);
+            }
         }
-     
     }
 }
