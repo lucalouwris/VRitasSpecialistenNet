c1c0867ecddcddd7bb89895598a649f1847bd760
LDvanGrinsven <61006226+LDvanGrinsven@users.noreply.github.com>
2021-11-04 09:36:28 +0100

    Fixed the sign rotation towards the player.

--- Assets/Scripts/Interactions/SignRotation.cs
+++ Assets/Scripts/Interactions/SignRotation.cs
@@ -4,24 +4,18 @@ using UnityEngine;
 
 public class SignRotation : MonoBehaviour
 {
-    GameObject player;
-    // Start is called before the first frame update
-    void Start()
-    {
-        player = GameObject.Find("XR Rig");
-    }
-
+    [SerializeField] GameObject player;
     // Update is called once per frame
     void Update()
     {
         if(player != null)
         {
             float distance = Vector3.Distance(this.transform.position, player.transform.position);
-            if (distance < 1)
+            if (distance < 25)
             {
-                Vector3 targetPostition = new Vector3(-player.transform.position.x,
+                Vector3 targetPostition = new Vector3(player.transform.position.x,
                                            this.transform.position.y,
-                                           -player.transform.position.z);
+                                           player.transform.position.z);
                 this.transform.LookAt(targetPostition);
             }
         }
