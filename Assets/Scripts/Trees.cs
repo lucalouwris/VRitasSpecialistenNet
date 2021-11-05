88b5e32be565e31e46147c943bbecb7e111eedc0
Luca Louwris <luca.louwris@gmail.com>
2021-11-05 09:23:06 +0100

    Small changes

--- Assets/Scripts/Trees.cs
+++ Assets/Scripts/Trees.cs
@@ -6,7 +6,7 @@ public class Trees : MonoBehaviour
 {
     void Start()
     {
-        Quaternion target = Quaternion.Euler(0, Random.Range(0.0f, 90.0f), 0);
+        Quaternion target = Quaternion.Euler(0, Random.Range(-180f, 180f), 0);
         this.transform.rotation = target;
     }
 }
