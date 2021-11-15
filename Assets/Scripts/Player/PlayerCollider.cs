1613a51ab4accf87e099dd36c633a4aad5e8dc49
Luca Louwris <luca.louwris@gmail.com>
2021-11-15 09:10:56 +0100

    Changed grab to be direct instead of raycast

--- Assets/Scripts/Player/PlayerCollider.cs
+++ Assets/Scripts/Player/PlayerCollider.cs
@@ -9,7 +9,7 @@ public class PlayerCollider : MonoBehaviour
     [SerializeField] private GameObject playerHead;
     private void FixedUpdate()
     {
-        Vector3 playerOffset = playerHead.transform.position - transform.position ;
+        Vector3 playerOffset = playerHead.transform.position - transform.position;
         playerOffset.y = playerHead.transform.position.y / 2;
         collider.center = playerOffset;
         collider.height = playerHead.transform.position.y;
