82cdb96e3db90127ef9a0efcbbec10129377fb6e
DESKTOP-DVO1LLP\VR Student 1 <brian.bawuah@hva.nl>
2021-11-23 15:47:28 +0100

    Script fix, got the watch to rotate properly

--- Assets/Scripts/PDAController.cs
+++ Assets/Scripts/PDAController.cs
@@ -3,27 +3,21 @@ using UnityEngine;
 public class PDAController : MonoBehaviour
 {
     [SerializeField] private Transform clockHandTransform;
-    public float currRotation;
-    private float rotation;
+    private float currRotation;
+    private float rotation = 90;
     public static float rotationSpeed = 1f;
 
 
     private void Update()
     {
-        currRotation = clockHandTransform.rotation.eulerAngles.z;
-        Debug.Log(currRotation);
-        
-        if (currRotation <= 315 && currRotation > 180)
+        currRotation = clockHandTransform.localEulerAngles.z;
+
+        if ((currRotation <= 315 && currRotation > 180) || (currRotation > 45 && currRotation < 180))
         {
-            rotation = 90f;
-        }
-        else if (currRotation > 45 && currRotation < 180)
-        {
-            rotation = -45f;
+            rotation *= -1;        
         }
 
-        clockHandTransform.eulerAngles += new Vector3(0, 0, (Time.deltaTime * rotation) / rotationSpeed);
-        
+        clockHandTransform.localEulerAngles += new Vector3(0, 0, (Time.deltaTime * rotation) / rotationSpeed);
     }
 
 
