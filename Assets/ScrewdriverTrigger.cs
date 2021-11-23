91b6304334ded568788d10759ecfd1852162a5b3
Fabi <fs126@hdm-stuttgart.de>
2021-11-23 11:37:59 +0100

    added mechanics to check for label if trigger is activated, then applied roatation to the powercell and it can now be fixed by putting the screw driver close to the broken screw 🔩

---
+++ Assets/ScrewdriverTrigger.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrewdriverTrigger : MonoBehaviour
+{
+    public string tagToCheck;
+    private GameObject powerCell;
+    private bool hasbeenFixed = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        powerCell = GameObject.Find("powercell");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //This method checks if the colliding obejct has a specific tag
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collision deteced; checking for this tag:" + tagToCheck);
+        if (other.gameObject.CompareTag(tagToCheck))
+        {
+            if (!hasbeenFixed)
+            {
+                TransformPowerCell();
+            }
+        }
+    }
+
+    //Transforms the Powercell into the "repaired" position
+    private void TransformPowerCell()
+    {
+        Debug.Log("Trying to apply Rotation and Transform");
+        powerCell.transform.Rotate(0f, 0f, 7f, Space.World);
+
+        hasbeenFixed = true;
+
+    }
+
+    //Need this for the lever to check if the machine has been fixed or nah
+    public bool getHasBeenFixed()
+    {
+        return hasbeenFixed;
+    }
+}
