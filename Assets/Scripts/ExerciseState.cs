86acd53b9946d028ae4b13b9e6fb6665f92132d1
LDvanGrinsven <61006226+LDvanGrinsven@users.noreply.github.com>
2021-11-24 15:15:23 +0100

    Added the breathing exercise as a new state. Changed font to something more fitting.

--- Assets/Scripts/ExerciseState.cs
+++ Assets/Scripts/ExerciseState.cs
@@ -3,47 +3,117 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ExerciseState : MonoBehaviour
+public class ExerciseState : BaseState
 {
     [SerializeField] Image leftBar;
     [SerializeField] Image rightBar;
     [SerializeField] Text breathStat;
-    [SerializeField] Text breathStatNumber;
 
-    float time = 100;
+    [SerializeField] string prepTxt, breathInTxt, breathOutTxt, pauseTxt;
+
+    [SerializeField] private Transform playerTransform;
+    [SerializeField] private float distanceFromPlayer = 3.0f;
+    [SerializeField] private GameObject canvasObject;
+
+    float prepTime = 5f;
+    float outTime = 4f;
+    float inTime = 2.5f;
+    float pauseTime = 1f;
     float timeRemaining;
-    bool timerIsRunning = false;
-    // Start is called before the first frame update
-    public void UpdateExercise()
+    float startTime;
+    bool startedExercise = false;
+
+    private Vector3 goalPos;
+    private Vector3 offset;
+    [HideInInspector] public enum Stages { preparation, breathIn, breathPause, breathOut, completed };
+    Stages currentStage;
+    Stages previousStage;
+
+    public override void OnEnable()
     {
+        base.OnEnable();
+        offset = Vector3.right + Vector3.up * .4f;
+        currentStage = Stages.preparation;
+    }
+    public void SwitchExercise(float time, string activeText, Stages nextStage)
+    {
+        breathStat.text = activeText;
         timeRemaining = time;
-        timerIsRunning = true;
+        startTime = time;
+        previousStage = currentStage;
+        currentStage = nextStage;
+        startedExercise = true;
     }
 
     // Update is called once per frame
-    void Update()
+    public override void Update()
     {
-        leftBar.fillAmount -= 1.0f / timeRemaining;
-        rightBar.fillAmount -= 1.0f / timeRemaining;
-        if (timerIsRunning)
+        //Calculating preferred position.
+        goalPos = playerTransform.position + playerTransform.forward * distanceFromPlayer + offset;
+        transform.position = Vector3.MoveTowards(transform.position, goalPos, .5f * Time.deltaTime);
+        transform.LookAt(playerTransform);
+
+        if (Vector3.Distance(transform.position, goalPos) < .4f)
+        {
+            if (!startedExercise)
+                SwitchExercise(prepTime, prepTxt, Stages.preparation);
+            UpdateExercise();
+        }
+    }
+   
+    void UpdateExercise()
+    {
+        canvasObject.SetActive(true);
+
+        if (currentStage == Stages.breathIn)
+        {
+            leftBar.fillAmount = 1.0f - (timeRemaining / startTime);
+            rightBar.fillAmount = 1.0f - (timeRemaining / startTime);
+        }
+        else if (currentStage == Stages.breathPause || currentStage == Stages.preparation)
+        {
+            leftBar.fillAmount = leftBar.fillAmount;
+            rightBar.fillAmount = rightBar.fillAmount;
+        }
+        else
+        {
+            leftBar.fillAmount = (timeRemaining / startTime);
+            rightBar.fillAmount = (timeRemaining / startTime);
+        }
+
+        if (startedExercise)
         {
             if (timeRemaining > 0)
-            {
                 timeRemaining -= Time.deltaTime;
-                DisplayTime(timeRemaining);
-            }
             else
             {
-
+                switch (currentStage)
+                {
+                    case Stages.preparation:
+                        SwitchExercise(inTime, breathInTxt, Stages.breathIn);
+                        break;
+                    case Stages.breathIn:
+                        SwitchExercise(pauseTime, pauseTxt, Stages.breathPause);
+                        break;
+                    case Stages.breathOut:
+                        SwitchExercise(pauseTime, pauseTxt, Stages.breathPause);
+                        break;
+                    case Stages.breathPause:
+                        if (previousStage == Stages.breathIn) 
+                            SwitchExercise(outTime, breathOutTxt, Stages.breathOut);
+                        else 
+                            SwitchExercise(inTime, breathInTxt, Stages.breathIn);
+                        break;
+                    case Stages.completed:
+                        break;
+                }
             }
         }
     }
-    void DisplayTime(float timeToDisplay)
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    public override void OnDisable()
     {
-        timeToDisplay += 1;
-
-        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
-
-        breathStatNumber.text = string.Format("{0:0}", seconds);
+        canvasObject.SetActive(false);
     }
 }
