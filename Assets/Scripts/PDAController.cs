da3dabb75b253b5e9fad3a59e29c75d9f01e84f7
trymstenberg <55834832+trymstenberg@users.noreply.github.com>
2021-11-24 16:03:55 +0100

    Optimize code and changed some values

--- Assets/Scripts/PDAController.cs
+++ Assets/Scripts/PDAController.cs
@@ -4,16 +4,16 @@ public class PDAController : MonoBehaviour
 {
     [SerializeField] private Transform clockHandTransform;
     private float currRotation;
-    private static float rotation = 90;     //90 degrees a secont
-    public static float rotationSpeed = 5; //Number of seconds it takes to complete 90 degrees
-    private static float resetTime = 20;
-    private static float countdownTime;
-    private float test = 20;
-    private static bool Moving = true;
+    private float rotation = 90;     //90 degrees a secont
+    public float rotationSpeed = 5; //Number of seconds it takes to complete 90 degrees
+    private float resetTime = 20;
+    private float countdownTime;
+    //private float test = 20;
+    private bool Moving = true;
 
     private void Start()
     {
-        countdownTime = 10;
+        countdownTime = 600;
     }
 
     private void Update()
@@ -34,7 +34,7 @@ public class PDAController : MonoBehaviour
             
         } else if (currRotation > 45 && currRotation < 180) //On full
         {
-            rotation = -90;
+            rotation = -90;                                 //Change the direction of the rotation
             rotationSpeed = countdownTime;                  //Change the speed to the count down time
             Moving = false;                                 //Set the moving to false so the arrow stops when its empty
         }
@@ -43,23 +43,20 @@ public class PDAController : MonoBehaviour
         clockHandTransform.localEulerAngles += new Vector3(0, Time.deltaTime * rotation / rotationSpeed, 0);
 
         //This is just to test that mu function works
-        Debug.Log(rotationSpeed);
+        /*Debug.Log(rotationSpeed);
         test -= Time.deltaTime;
         if (test < 0)
         {
             test = 30;
             ResetClock(10);
-        }
+        }*/
         
     }
 
-    public static void ResetClock(float timeUntilEmpty)
+    public void ResetClock(float timeUntilEmpty)
     {
         countdownTime = timeUntilEmpty;
         rotationSpeed = resetTime;
         Moving = true;
     }
-
-
-    
 }
