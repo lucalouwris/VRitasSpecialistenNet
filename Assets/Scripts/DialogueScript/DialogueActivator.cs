cc027b837ef2c994017c72e3ea99bfddcce13a38
bbawuah <brianbawuah@icloud.com>
2021-12-02 13:59:51 +0100

    remove stateMachine from box collidor

--- Assets/Scripts/DialogueScript/DialogueActivator.cs
+++ Assets/Scripts/DialogueScript/DialogueActivator.cs
@@ -5,6 +5,27 @@ public class DialogueActivator : MonoBehaviour, IInteractable
 
     [SerializeField] private DialogueObject dialogueObject;
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player") && other.TryGetComponent(out PlayerController player))
+        {
+            player.Interactable = this;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+
+        if (other.CompareTag("Player") && other.TryGetComponent(out PlayerController player))
+        {
+           if(player.Interactable is DialogueActivator dialogueActivator && dialogueActivator == this)
+            {
+                player.Interactable = null;
+            }
+        }
+
+    }
+
     public void Interact(PlayerController player)
     {
         GetComponent<StateMachine>().SwitchState(GetComponent<StateMachine>().States[1]);
