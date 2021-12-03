cf9812e112fbd99635e6c93f7bd71527a5b2a851
LDvanGrinsven <61006226+LDvanGrinsven@users.noreply.github.com>
2021-12-03 11:21:01 +0100

    Removed 2 bools which made no sense.

--- Assets/Scripts/BrianPointer.cs
+++ Assets/Scripts/BrianPointer.cs
@@ -9,8 +9,6 @@ public class BrianPointer : MonoBehaviour
     public Transform target;
     public Vector3 offset;
     [SerializeField] Camera cam;
-    [HideInInspector] public bool blink = true;
-    [HideInInspector] public bool active = true;
     [SerializeField] float blinkInterval = 0.5f;
     float currentInterval;
 
@@ -21,46 +19,39 @@ public class BrianPointer : MonoBehaviour
 
     private void Update()
     {
-        if (active)
+        float minX = img.GetPixelAdjustedRect().width / 2;
+        float maxX = Screen.width - minX;
+
+        float minY = img.GetPixelAdjustedRect().height / 2;
+        float maxY = Screen.height - minY;
+
+        Vector2 pos = cam.WorldToScreenPoint(target.position + offset);
+        float distance = Vector3.Distance(target.position, transform.position);
+
+        img.rectTransform.localScale = new Vector3(1f / (distance / 2f), 1f / (distance / 2f), 1f);
+        if (Vector3.Dot((target.position - transform.position), transform.forward) < 0)
         {
-            float minX = img.GetPixelAdjustedRect().width / 2;
-            float maxX = Screen.width - minX;
+            if (pos.x < Screen.width / 2)
+                pos.x = maxX;
+            else
+                pos.x = minX;
+            pos.y = minY;
+        }
+        pos.x = Mathf.Clamp(pos.x, minX, maxX);
+        pos.y = Mathf.Clamp(pos.y, minY, maxY);
 
-            float minY = img.GetPixelAdjustedRect().height / 2;
-            float maxY = Screen.height - minY;
+        img.transform.position = pos;
 
-            Vector2 pos = cam.WorldToScreenPoint(target.position + offset);
-            float distance = Vector3.Distance(target.position, transform.position);
+        if (currentInterval > 0)
+            currentInterval -= Time.deltaTime;
+        else
+        {
+            if (img.color.a == 1f)
+                img.color = new Color(img.color.r, img.color.g, img.color.b, 0);
+            else
+                img.color = new Color(img.color.r, img.color.g, img.color.b, 1);
 
-            img.rectTransform.localScale = new Vector3(1f / (distance / 2f), 1f / (distance / 2f), 1f);
-            if (Vector3.Dot((target.position - transform.position), transform.forward) < 0)
-            {
-                if (pos.x < Screen.width / 2)
-                    pos.x = maxX;
-                else
-                    pos.x = minX;
-                pos.y = minY;
-            }
-            pos.x = Mathf.Clamp(pos.x, minX, maxX);
-            pos.y = Mathf.Clamp(pos.y, minY, maxY);
-
-            img.transform.position = pos;
-
-
-            if (blink)
-            {
-                if (currentInterval > 0)
-                    currentInterval -= Time.deltaTime;
-                else
-                {
-                    if (img.color.a == 1f)
-                        img.color = new Color(img.color.r, img.color.g, img.color.b, 0);
-                    else
-                        img.color = new Color(img.color.r, img.color.g, img.color.b, 1);
-
-                    currentInterval = blinkInterval;
-                }
-            }
+            currentInterval = blinkInterval;
         }
     }
 }
