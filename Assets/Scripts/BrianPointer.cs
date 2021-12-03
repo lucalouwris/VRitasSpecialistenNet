4bc80160f4520739bbb2a929ddfd06ba53cc2243
LDvanGrinsven <61006226+LDvanGrinsven@users.noreply.github.com>
2021-12-03 11:30:33 +0100

    Added a bool back as the pointer needs to be hidden when not active. Fixed bug where the bool was not set properly

--- Assets/Scripts/BrianPointer.cs
+++ Assets/Scripts/BrianPointer.cs
@@ -9,49 +9,58 @@ public class BrianPointer : MonoBehaviour
     public Transform target;
     public Vector3 offset;
     [SerializeField] Camera cam;
+    [HideInInspector] public bool active;
     [SerializeField] float blinkInterval = 0.5f;
     float currentInterval;
 
     private void Start()
     {
+        active = false;
         currentInterval = blinkInterval;
     }
 
     private void Update()
     {
-        float minX = img.GetPixelAdjustedRect().width / 2;
-        float maxX = Screen.width - minX;
-
-        float minY = img.GetPixelAdjustedRect().height / 2;
-        float maxY = Screen.height - minY;
-
-        Vector2 pos = cam.WorldToScreenPoint(target.position + offset);
-        float distance = Vector3.Distance(target.position, transform.position);
-
-        img.rectTransform.localScale = new Vector3(1f / (distance / 2f), 1f / (distance / 2f), 1f);
-        if (Vector3.Dot((target.position - transform.position), transform.forward) < 0)
+        if (active)
         {
-            if (pos.x < Screen.width / 2)
-                pos.x = maxX;
-            else
-                pos.x = minX;
-            pos.y = minY;
+            if (!img.gameObject.activeSelf)
+                img.gameObject.SetActive(true);
+            float minX = img.GetPixelAdjustedRect().width / 2;
+            float maxX = Screen.width - minX;
+
+            float minY = img.GetPixelAdjustedRect().height / 2;
+            float maxY = Screen.height - minY;
+
+            Vector2 pos = cam.WorldToScreenPoint(target.position + offset);
+            float distance = Vector3.Distance(target.position, transform.position);
+
+            img.rectTransform.localScale = new Vector3(1f / (distance / 2f), 1f / (distance / 2f), 1f);
+            if (Vector3.Dot((target.position - transform.position), transform.forward) < 0)
+            {
+                if (pos.x < Screen.width / 2)
+                    pos.x = maxX;
+                else
+                    pos.x = minX;
+                pos.y = minY;
+            }
+            pos.x = Mathf.Clamp(pos.x, minX, maxX);
+            pos.y = Mathf.Clamp(pos.y, minY, maxY);
+
+            img.transform.position = pos;
+
+                if (currentInterval > 0)
+                    currentInterval -= Time.deltaTime;
+                else
+                {
+                    if (img.color.a == 1f)
+                        img.color = new Color(img.color.r, img.color.g, img.color.b, 0);
+                    else
+                        img.color = new Color(img.color.r, img.color.g, img.color.b, 1);
+
+                    currentInterval = blinkInterval;
+                }
         }
-        pos.x = Mathf.Clamp(pos.x, minX, maxX);
-        pos.y = Mathf.Clamp(pos.y, minY, maxY);
-
-        img.transform.position = pos;
-
-        if (currentInterval > 0)
-            currentInterval -= Time.deltaTime;
         else
-        {
-            if (img.color.a == 1f)
-                img.color = new Color(img.color.r, img.color.g, img.color.b, 0);
-            else
-                img.color = new Color(img.color.r, img.color.g, img.color.b, 1);
-
-            currentInterval = blinkInterval;
-        }
+            img.gameObject.SetActive(false);
     }
 }
