f3d06e0f8e0a7ac63d6ce5657c56d656a4ccf3cb
DESKTOP-DVO1LLP\VR Student 1 <brian.bawuah@hva.nl>
2021-12-10 10:05:54 +0100

    fixed dialogue lekker! and  made prefab lekker gewert!

--- Assets/Scripts/DialogueScript/DialogueActivator.cs
+++ Assets/Scripts/DialogueScript/DialogueActivator.cs
@@ -7,15 +7,20 @@ public class DialogueActivator : MonoBehaviour
     [SerializeField] private StateMachine brian;
     [SerializeField] private BrianSays speaker;
     [SerializeField] private Vector3 position;
+    [SerializeField] private bool shouldDisable = true;
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Player") || other.CompareTag("Lever") || other.CompareTag("brokenPiece"))
+        if (other.CompareTag("Player") || other.CompareTag("Lever") || other.CompareTag("brokenPiece"))
         {
-                brian.transform.position = position;
-                Debug.Log("switchState");
-                brian.SwitchState(brian.States[1]);
+            brian.transform.position = position;
+            Debug.Log("switchState");
+            brian.SwitchState(brian.States[1]);
+            if (shouldDisable)
+            {
                 this.gameObject.SetActive(false);
+            }
+
 
             this.speaker.playThis = dialogueObject;
         }
