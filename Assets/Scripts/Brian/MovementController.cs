6bd6064bf4cf4948e76d6b544272664dcbd0340d
Luca Louwris <luca.louwris@gmail.com>
2021-12-16 10:24:37 +0100

    Changed agaaiin but now it works

--- Assets/Scripts/Brian/MovementController.cs
+++ Assets/Scripts/Brian/MovementController.cs
@@ -6,11 +6,9 @@ using UnityEngine.Animations.Rigging;
 
 public class MovementController : MonoBehaviour
 {
-    [SerializeField] Transform PointerTarget;    
-    [SerializeField] MultiAimConstraintData multiAimConstraintHand;
-    [SerializeField] MultiAimConstraintData multiAimConstraintHead;
-    [SerializeField] MultiAimConstraintData multiAimConstraintBody;
-    [SerializeField] float bodyWeightFactor = 0.6f;
+    [SerializeField] Transform PointerTarget;
+    [SerializeField] Rig LookAt;
+    [SerializeField] Rig PointAt;
 
     public void ChangePoint(Vector3 newPosition)
     {
@@ -18,54 +16,57 @@ public class MovementController : MonoBehaviour
         PointerTarget.position = newPosition;
     }
 
-    public void ResetPoint()
+    public void ResetPoint(Rig rig)
     {
         //Sets the weight of the pointing to 0 the arm just runs the animation
-        WeightedTransformArray weightedTransforms = multiAimConstraintHand.sourceObjects;
-        WeightedTransform weighted = weightedTransforms[0];
-        weighted.weight = 0f;
-        weightedTransforms[0] = weighted;
-        multiAimConstraintHand.sourceObjects = weightedTransforms;
+        rig.weight = 0f;
+        // WeightedTransformArray weightedTransforms = multiAimConstraintHand.sourceObjects;
+        // WeightedTransform weighted = weightedTransforms[0];
+        // weighted.weight = 0f;
+        // weightedTransforms[0] = weighted;
+        // multiAimConstraintHand.sourceObjects = weightedTransforms;
     }
 
-    public void AddPoint()
+    public void AddPoint(Rig rig)
     {
         //Sets the weight of the pointing to 1
-        WeightedTransformArray weightedTransforms = multiAimConstraintHand.sourceObjects;
-        WeightedTransform weighted = weightedTransforms[0];
-        weighted.weight = 1f;
-        weightedTransforms[0] = weighted;
-        multiAimConstraintHand.sourceObjects = weightedTransforms;
+        rig.weight = 1f;
+        // WeightedTransformArray weightedTransforms = multiAimConstraintHand.sourceObjects;
+        // WeightedTransform weighted = weightedTransforms[0];
+        // weighted.weight = 1f;
+        // weightedTransforms[0] = weighted;
+        // multiAimConstraintHand.sourceObjects = weightedTransforms;
     }
 
-    public void changeLook(float weight)
-    {
-        if(weight > 1f)
-        {
-            weight = 1f;
-        } else if (weight<0f)
-        {
-            weight = 0f;
-        }
-
-        //Changing the weight of Brian looking at the player
-        WeightedTransformArray weightedTransformsHead = multiAimConstraintHead.sourceObjects;
-        WeightedTransform weightedHead = weightedTransformsHead[0];
-        weightedHead.weight = weight;
-        weightedTransformsHead[0] = weightedHead;
-        multiAimConstraintHead.sourceObjects = weightedTransformsHead;
-
-        //Change the weight of the weight of the body as a percentage of the weight of the head
-        float bodyweight = weight * bodyWeightFactor;
-
-        WeightedTransformArray weightedTransformsBody = multiAimConstraintBody.sourceObjects;
-        WeightedTransform weightedBody = weightedTransformsBody[0];
-        weightedBody.weight = bodyweight;
-        weightedTransformsBody[0] = weightedBody;
-        multiAimConstraintBody.sourceObjects = weightedTransformsBody;
-    }
+    // public void changeLook(float weight)
+    // {
+    //     if(weight > 1f)
+    //     {
+    //         weight = 1f;
+    //     } else if (weight<0f)
+    //     {
+    //         weight = 0f;
+    //     }
+    //
+    //     //Changing the weight of Brian looking at the player
+    //     WeightedTransformArray weightedTransformsHead = multiAimConstraintHead.sourceObjects;
+    //     WeightedTransform weightedHead = weightedTransformsHead[0];
+    //     weightedHead.weight = weight;
+    //     weightedTransformsHead[0] = weightedHead;
+    //     multiAimConstraintHead.sourceObjects = weightedTransformsHead;
+    //
+    //     //Change the weight of the weight of the body as a percentage of the weight of the head
+    //     float bodyweight = weight * bodyWeightFactor;
+    //
+    //     WeightedTransformArray weightedTransformsBody = multiAimConstraintBody.sourceObjects;
+    //     WeightedTransform weightedBody = weightedTransformsBody[0];
+    //     weightedBody.weight = bodyweight;
+    //     weightedTransformsBody[0] = weightedBody;
+    //     multiAimConstraintBody.sourceObjects = weightedTransformsBody;
+    // }
 
     // Update is called once per frame
+    
     [ContextMenu("Change Point")]
     void RandomPoint()
     {
@@ -75,18 +76,24 @@ public class MovementController : MonoBehaviour
     [ContextMenu("Set Pointing")]
     void SetPoint()
     {
-        AddPoint();
+        AddPoint(PointAt);
     }
 
-    [ContextMenu("Reset Pointing")]
+    [ContextMenu("Clear Pointing")]
     void RemovePoint()
     {
-        ResetPoint();
+        ResetPoint(PointAt);
     }
 
-    [ContextMenu("Change looking")]
-    void ModifyPoint(MenuCommand command)
+    [ContextMenu("Set looking")]
+    void ModifyPoint()
     {
-        changeLook(command);
+        AddPoint(LookAt);
+    }
+    
+    [ContextMenu("Clear looking")]
+    void RemoveLook()
+    {
+        ResetPoint(LookAt);
     }
 }
