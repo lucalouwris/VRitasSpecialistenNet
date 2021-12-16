dfb3c332290cb341e47d70a2b38798e74ece17bb
LDvanGrinsven <61006226+LDvanGrinsven@users.noreply.github.com>
2021-12-16 13:38:52 +0100

    Testing Fadein

--- Assets/Scripts/Ship/EndingTransition.cs
+++ Assets/Scripts/Ship/EndingTransition.cs
@@ -5,21 +5,25 @@ using UnityEngine;
 public class EndingTransition : MonoBehaviour
 {
     [SerializeField] private Animator anim;
+    [SerializeField] private Animator transition;
+    [SerializeField] GameObject player;
     bool isFlying = false;
     private void Update()
     {
         if (anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 1 && isFlying)
         {  //If normalizedTime is 0 to 1 means animation is playing, if greater than 1 means finished
             CreditsTransition();
+            isFlying = false;
         }
     }
     public void Fly()
     {
+        player.transform.SetParent(this.transform);
         anim.SetTrigger("FlyTheShip");
         isFlying = true;
     }
     public void CreditsTransition()
     {
-
+        transition.SetTrigger("Fade");
     }
 }
