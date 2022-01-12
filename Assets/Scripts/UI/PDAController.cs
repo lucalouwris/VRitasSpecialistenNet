0fd072330449fbaa7074a53570b1f5bc35fd3972
trymstenberg <55834832+trymstenberg@users.noreply.github.com>
2022-01-12 15:53:21 +0100

    Added comments, removed debug

--- Assets/Scripts/UI/PDAController.cs
+++ Assets/Scripts/UI/PDAController.cs
@@ -10,7 +10,7 @@ public class PDAController : MonoBehaviour
     [SerializeField] public float rotationSpeed = 800; //Number of seconds it takes to complete 90 degrees
     [SerializeField] float feedbackStrength = 0.5f;
     [SerializeField] float feedbackLength = 0.25f;
-    [SerializeField] float wait = 0.7f;
+    [SerializeField] float wait = 0.7f;                 //Wait between pulsations
 
     private float currRotation;
     private float rotation = 90;     //90 degrees a second
@@ -24,7 +24,7 @@ public class PDAController : MonoBehaviour
     {
         currRotation = clockHandTransform.localEulerAngles.y;
 
-        if (!(currRotation <= 325 && currRotation > 180))
+        if (!(currRotation <= 325 && currRotation > 180)) //If the indicator is not empty
         {
             clockHandTransform.localEulerAngles -= new Vector3(0, Time.deltaTime * rotation / rotationSpeed, 0);
         } else
@@ -37,17 +37,15 @@ public class PDAController : MonoBehaviour
     {
         while (true)
         {
-            if (empty)
+            if (empty) //If the indicator is empty, start pulsation on controller to increase stress
             {
                 controller.SendHapticImpulse(feedbackStrength, feedbackLength);
-               if(ReadHeartrate.currPulse != 0)
+               if(ReadHeartrate.currPulse > 0) //Heartrate is set to 0 if it is not received in the other, this also ensures other errors like Null
                 {
-                    Debug.Log("Current heartrate is: " + ReadHeartrate.currPulse);
                     yield return new WaitForSeconds(feedbackLength + (60/ReadHeartrate.currPulse));
                 } else
                 {
                     yield return new WaitForSeconds(feedbackLength + wait);
-                    Debug.Log("Cant find heartrate");
                 }
             } else
             {
@@ -55,7 +53,7 @@ public class PDAController : MonoBehaviour
             }
         }
     }
-    public void fillOxygen()
+    public void fillOxygen() //Set the indicator to the top again
     {
         clockHandTransform.localEulerAngles = new Vector3(0, 45, 0);
         empty = false;
